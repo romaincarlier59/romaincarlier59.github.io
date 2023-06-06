849d65b230de646bec9f554b3b777daf6f1eb682
romaincarlier <rcretail@gmx.com>
2023-06-06 10:19:26 +0200

    MAJ

--- Plateforme Musicale/script2.js
+++ Plateforme Musicale/script2.js
@@ -1,7 +1,24 @@
 var logo = document.querySelector('.hamburger');
 var menu = document.querySelector('.menu');
+var music = document.querySelector('.music');
+var player = document.querySelector('.player');
+var playerImage = document.querySelector('.imgBox');
+var podcasts = document.querySelector('.podcasts');
+var favoris = document.querySelector('.favoris');
+var recherche = document.querySelector('.recherche');
+var premium = document.querySelector('.premium');
 
-logo.addEventListener('click, function()')
-{
+
+logo.addEventListener("click", function(){
     menu.classList.toggle('showmenu')
-};
+});
+
+music.addEventListener("click", function(){
+    player.classList.add("visible");
+    playerImage.classList.add("visible");
+        
+})
+podcasts.addEventListener("click", function () {
+    player.classList.remove("visible");
+    playerImage.classList.remove("visible");
+})
