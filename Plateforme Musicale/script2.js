bc1b0cdbd67f57fe508cfffe62bfa5fcd47d3865
romaincarlier <rcretail@gmx.com>
2023-06-07 14:44:48 +0200

    work

--- Plateforme Musicale/script2.js
+++ Plateforme Musicale/script2.js
@@ -1,24 +1,21 @@
-var logo = document.querySelector('.hamburger');
-var menu = document.querySelector('.menu');
-var music = document.querySelector('.music');
-var player = document.querySelector('.player');
-var playerImage = document.querySelector('.imgBox');
-var podcasts = document.querySelector('.podcasts');
-var favoris = document.querySelector('.favoris');
-var recherche = document.querySelector('.recherche');
-var premium = document.querySelector('.premium');
+var logo = document.querySelector(".hamburger");
+var menu = document.querySelector(".menu");
+var music = document.querySelector(".music");
+var player = document.querySelector(".player");
+var playerImage = document.querySelector(".imgBox");
+var podcasts = document.querySelector(".podcasts");
+var recherche = document.querySelector(".recherche");
 
 
-logo.addEventListener("click", function(){
-    menu.classList.toggle('showmenu')
+logo.addEventListener("click", function () {
+	menu.classList.toggle("showmenu");
 });
 
-music.addEventListener("click", function(){
-    player.classList.add("visible");
-    playerImage.classList.add("visible");
-        
-})
+music.addEventListener("click", function () {
+	player.classList.add("visible");
+	playerImage.classList.add("visible");
+});
 podcasts.addEventListener("click", function () {
-    player.classList.remove("visible");
-    playerImage.classList.remove("visible");
-})
+	player.classList.remove("visible");
+	playerImage.classList.remove("visible");
+});
